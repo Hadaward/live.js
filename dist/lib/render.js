0ed4cf40ebe90a70af05fac3f7e92c0e16316267
Eduardo Gimenez <24706265+Hadaward@users.noreply.github.com>
2024-03-15 11:37:52 -0300

    Delete dist directory

--- dist/lib/render.js
+++
@@ -1,9 +0,0 @@
-export class Live {
-    constructor(callback, usingStates = []) {
-        this.callback = callback;
-        this.usingStates = Object.freeze([...usingStates]);
-    }
-}
-export function live(callback, usingStates = []) {
-    return new Live(callback, usingStates);
-}
